8f93d8b6ee8fe825bcc171e91ef383d2f7d72d20
Adit Hernowo <adith@Adits-MacBook-Air.local>
2023-06-01 00:45:26 +0700

    create register screen

--- app/src/main/java/com/example/fashionday/LoginActivity.kt
+++ app/src/main/java/com/example/fashionday/LoginActivity.kt
@@ -1,15 +1,28 @@
 package com.example.fashionday
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fashionday.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
         setupHyperlink()
+
+
     }
 
     private fun setupHyperlink() {
